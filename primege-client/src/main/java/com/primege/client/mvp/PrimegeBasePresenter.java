79fc8bedb68d69c940edbc2ac224438939199fd8
Philippe AMELINE <philippe@ameline.net>
2024-03-25 21:47:29 +0100

    Checking commit

--- primege-client/src/main/java/com/primege/client/mvp/PrimegeBasePresenter.java
+++ primege-client/src/main/java/com/primege/client/mvp/PrimegeBasePresenter.java
@@ -8,39 +8,38 @@ import net.customware.gwt.dispatch.client.DispatchAsync;
 import net.customware.gwt.presenter.client.EventBus;
 import net.customware.gwt.presenter.client.widget.WidgetPresenter;
 
-public abstract class PrimegeBasePresenter<D extends PrimegeBaseInterface> extends WidgetPresenter<D> 
+public abstract class PrimegeBasePresenter<D extends PrimegeBaseInterface> extends WidgetPresenter<D>
 {
-	private       final DispatchAsync          _dispatcher ;
-	private       final PrimegeSupervisorModel _supervisor ;
-	
-	protected           String _sPredefinedCity ;
-			
-	@Inject
-	public PrimegeBasePresenter(D                           display, 
-							               EventBus                     eventBus,
-							               final DispatchAsync          dispatcher,
-							               final PrimegeSupervisorModel supervisor) 
-	{
-		super(display, eventBus) ;
-		
-		_dispatcher = dispatcher ;
-		_supervisor = supervisor ;
-		
-		_sPredefinedCity = "" ;
-			
-		bind() ;		
-	}
-	
-	@Override
-	protected void onBind()
-	{
-	}
-			
-	@Override
-	protected void onUnbind() {
-	}
+    private final DispatchAsync          _dispatcher;
+    private final PrimegeSupervisorModel _supervisor;
 
-	@Override
-	public void revealDisplay() {
-	}
+    protected String                     _sPredefinedCity;
+
+    @Inject
+    public PrimegeBasePresenter(D display, EventBus eventBus, final DispatchAsync dispatcher, final PrimegeSupervisorModel supervisor)
+    {
+        super(display, eventBus);
+
+        _dispatcher      = dispatcher;
+        _supervisor      = supervisor;
+
+        _sPredefinedCity = "";
+
+        bind();
+    }
+
+    @Override
+    protected void onBind()
+    {
+    }
+
+    @Override
+    protected void onUnbind()
+    {
+    }
+
+    @Override
+    public void revealDisplay()
+    {
+    }
 }
